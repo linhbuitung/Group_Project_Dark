c73c134309abce1ccf61f76d62bf53c7abe5f1b1
linhbuitung <linh.buitung18@gmail.com>
2021-12-13 16:07:57 +0100

    Basic Movement
    Add Character movement with acceleration and Jump physics

---
+++ Group_Project_Dark_Limbo/Assets/Script/Player/Player_Movement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    float Speed = 0;
+    public float MaxSpeed = 6f;
+    public float Acceleration = 7f;
+    public float Deceleration = 7f;
+    public float jumpVelocity = 6f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }   
+    void Update()
+    {
+        Move();
+        Jump();
+        Fall();
+    }
+
+    private void Move()
+    {
+        if ((Input.GetKey(KeyCode.D)) && (Speed < MaxSpeed))
+        {
+            Speed = Speed + Acceleration * Time.deltaTime;
+        }
+            
+        else if ((Input.GetKey(KeyCode.A)) && (Speed > -MaxSpeed))
+        {
+            Speed = Speed - Acceleration * Time.deltaTime;
+        }
+           
+        else
+        {
+            if (Speed > Deceleration * Time.deltaTime)
+                Speed = Speed - (Deceleration + 1) * Time.deltaTime;
+            else if (Speed < -Deceleration * Time.deltaTime)
+                Speed = Speed + (Deceleration + 1) * Time.deltaTime;
+            else
+                Speed = 0;
+        }
+        transform.position = new Vector2(transform.position.x + Speed * Time.deltaTime, transform.position.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.velocity = Vector2.up * jumpVelocity;
+        }
+        
+    }
+
+    private void Fall()
+    {
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            Debug.Log("Normal");
+        }
+        else if (body.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            Debug.Log("Low");
+        }
+    } 
+}
